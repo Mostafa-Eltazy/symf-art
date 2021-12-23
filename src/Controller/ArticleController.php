07732c5e3fb4e78f0038c6938c3d1a33eb257f5e
Mosta Eltazy <mostafa.eltazy@levelset.com>
2021-12-23 23:42:14 +0200

    twig updates & Json response

--- src/Controller/ArticleController.php
+++ src/Controller/ArticleController.php
@@ -1,17 +1,25 @@
 <?php
- namespace App\Controller;
+namespace App\Controller;
 
- use Symfony\Component\HttpFoundation\Response;
- use Symfony\Component\Routing\Annotation\Route;
- use Sensio\Bundle\FrameworkExtraBundle\Configuration\Method;
- use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
- class ArticleController extends AbstractController {
-     /**
-      * @Route("/")
-      *@Method({"GET"})
-      */
-     public function index(){
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Method;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\JsonResponse;
+
+class ArticleController extends AbstractController
+{
+    /**
+     * @Route("/")
+     *@Method({"GET"})
+     */
+    public function index()
+    {
         //  return new Response(('<html><body>hello</body></html>'));
-        return $this->render('articles/index.html.twig');
+        //  return $this->render('articles/index.html.twig', array('articles'=> $articles));
+        $articles = ['Articel 1', 'Article 2'];
+        return $this->json([
+            'articles' => $articles,
+        ]);
     }
- }
+}
