412a90d5f036fbbd53b121831ef09dd670a40b5f
Mosta Eltazy <mostafa.eltazy@levelset.com>
2021-12-25 00:57:26 +0200

    Basic Journal

---
+++ public/js/main.js
@@ -0,0 +1,16 @@
+const articles = document.getElementById('articles');
+console.log(articles);
+if (articles){
+    articles.addEventListener('click', e =>{
+        if(e.target.className === 'btn btn-danger delte-article'){
+            if(confirm('Are you sure ?')){
+                const id = e.target.getAttribute('data-id');
+
+                fetch(`/article/delete/${id}`, {
+                    method: 'DELETE'
+
+                }).then(res => window.location.reload())
+            }
+        }
+    })
+}
--- src/Controller/ArticleController.php
+++ src/Controller/ArticleController.php
@@ -3,23 +3,120 @@ namespace App\Controller;
 
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
-use Sensio\Bundle\FrameworkExtraBundle\Configuration\Method;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\JsonResponse;
+use App\Entity\Article;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\Extension\Core\Type\TextareaType;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
 
 class ArticleController extends AbstractController
 {
+    
     /**
-     * @Route("/")
-     *@Method({"GET"})
+     * @Route("/", name="articles_list")
      */
-    public function index()
+    public function index(Request $request)
     {
         //  return new Response(('<html><body>hello</body></html>'));
-        //  return $this->render('articles/index.html.twig', array('articles'=> $articles));
-        $articles = ['Articel 1', 'Article 2'];
-        return $this->json([
-            'articles' => $articles,
-        ]);
+        $articles = $this->getDoctrine()->getRepository(Article::class)->findAll();
+         return $this->render('articles/index.html.twig', array('articles'=> $articles));
+        // return $this->json([
+        //     'articles' => $articles
+        // ]);
     }
+    
+    /**
+     * @Route("article/new", name="new_article", methods={"GET","POST"})
+     */
+    public function new(Request $request){
+        $article = new Article();
+        
+        $form = $this->createFormBuilder($article)->add('title',TextType::class, array('attr'=>array('class'=>'form-control')))
+        ->add('body',TextareaType::class, array('required'=>true,'attr'=> array('class'=>'form-control')))
+        ->add('save', SubmitType::class, array('label'=>'Create','attr'=> array('class'=>'btn btn-primary mt-3')))
+        ->getForm();
+        
+        
+        $form->handleRequest($request);
+        if($form-> isSubmitted() && $form->isValid()) {
+            $article = $form->getData();
+            
+            $entityManager = $this->getDoctrine()->getManager();
+            $entityManager->persist($article);
+            $entityManager->flush();
+            
+            return $this->redirectToRoute('articles_list');
+        }
+        return $this->render('articles/new.html.twig', array('form'=> $form->createView()));
+    }
+
+    /**
+     * @Route("/article/{id}")
+     */
+    public function show($id){
+        $article = $this->getDoctrine()->getRepository(Article::class)->find($id);
+        return $this->render('articles/show.html.twig', array('article' => $article));
+        // console.log($article);
+        // return $this->json(['a'=>$article]);
+    }
+
+    
+    /**
+     * @Route("article/edit/{id}", name="edit_article",methods={"GET","POST"})
+     */
+    public function edit(Request $request, $id){
+        $article = new Article();
+        $article = $this->getDoctrine()->getRepository(Article::class)->find($id);
+
+        $form = $this->createFormBuilder($article)->add('title',TextType::class, array('attr'=>array('class'=>'form-control')))
+        ->add('body',TextareaType::class, array('required'=>true,'attr'=> array('class'=>'form-control')))
+        ->add('save', SubmitType::class, array('label'=>'Update','attr'=> array('class'=>'btn btn-primary mt-3')))
+        ->getForm();
+        
+        
+        $form->handleRequest($request);
+        if($form-> isSubmitted() && $form->isValid()) {
+           
+            
+            $entityManager = $this->getDoctrine()->getManager();
+            $entityManager->flush();
+            
+            return $this->redirectToRoute('articles_list');
+        }
+        return $this->render('articles/edit.html.twig', array('form'=> $form->createView()));
+    }
+
+    /**
+     * @Route("/article/delete/{id}", methods={"DELETE"})
+     */
+    public function delete(Request $request, $id){
+        $article = $this->getDoctrine()->getRepository(Article::class)->find($id);
+
+        $entityManager = $this->getDoctrine()->getManager();
+        $entityManager->remove($article);
+        $entityManager->flush();
+
+        $response = new Response();
+        $response->send();
+    }
+    
+    // /**
+    //  * @Route("/article/save")
+    //  */
+    // public function save(){
+    //     $entityManager = $this->getDoctrine()->getManager();
+    
+    //     $article = new Article();
+    //     $article->setTitle("1 article");
+    //     $article->setBody("this hte body of article 1");
+    
+    //     $entityManager->persist($article);
+    //     $entityManager->flush();
+    
+    //     return new Response('Saved an article id#'.$article->getId());
+    
+    
+    // }
 }
